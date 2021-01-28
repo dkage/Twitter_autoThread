d7c71e50c4837eba0b14e0928dec95cc37ce6f8e
Danilo Cunha <danilokage@gmail.com>
2021-01-28 04:46:11 -0300

    Add TODOs roadmap

--- reddit_handler.py
+++ reddit_handler.py
@@ -1,3 +1,6 @@
 import praw
 import requests
 
+# TODO create function to check the hottest threads from given sub
+# TODO create function to download video/gif/picture from given thread
+# TODO create function to grab all post info from given thread
--- twitter_handler.py
+++ twitter_handler.py
@@ -1,2 +1,6 @@
 from TwitterAPI import TwitterAPI as Twitter
 
+# TODO create function to post video/gif/picture
+# TODO create function to check video length
+# TODO create function to grab tweet ID for thread creation
+# TODO create function to crop video if necessary
